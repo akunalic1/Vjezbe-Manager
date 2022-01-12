dcf72588bf0297e19b6f51cbe3eb700f46b6e38e
akunalic1 <72765733+akunalic1@users.noreply.github.com>
2022-01-12 16:18:23 +0100

    Refaktor testova

--- public/js/test.js
+++ public/js/test.js
@@ -1,4 +1,3 @@
-
 chai.should();
 
 describe('Vjezbe Ajax Modul', function () {
@@ -15,99 +14,216 @@ describe('Vjezbe Ajax Modul', function () {
   afterEach(function () {
     this.xhr.restore();
   });
-  it('Testiranje posaljiPodatke 1', function (done) {
-    const data = {
-      brojVjezbi: 5,
-      brojZadataka: [1, 2, 3, 4, 5],
-    }
+  describe('Testiranje posaljiPodatke', function () {
 
-    let dataJson = JSON.stringify(data)
-    VjezbeAjax.posaljiPodatke(null, function (err, result) {
-      result.should.deep.equal(dataJson);
-      done();
-    });
+    it('Isparavan format', function (done) {
+      const data = {
+        brojVjezbi: 5,
+        brojZadataka: [1, 2, 3, 4, 5],
+      }
 
-    this.requests[0].respond(200, { 'Content-Type': 'text/json' }, dataJson);
-  });
-  it('Testiranje posaljiPodatke 2', function (done) {
-    let data = {
-      "brojVjezbi": 5,
-      "brojZadataka": [1,2,9,6,4,-1,0]
-    }
-    let error = {
-      "status": "error",
-      "data": "Pogrešan parametar z5,z6,brojZadataka"
-  }
-    VjezbeAjax.posaljiPodatke(data, function (err, result) {
-      chai.assert(result === null);
-      done();
-    });
-    this.requests[0].respond(400, { 'Content-Type': 'application/json' }, JSON.stringify(error));
-  });
-  it('Testiranje posaljiPodatke 3', function (done) {
-    let data = {
-      "brojVjezbi": 0,
-      "brojZadataka": [1,2,9,6,4,-1,0]
-    }
-    let error = {
-      "status": "error",
-      "data": "Pogrešan parametar brojVjezbi,z5,z6,brojZadataka"
-    }
-    VjezbeAjax.posaljiPodatke(data, function (err, result) {
-      chai.assert(result === null);
-      done();
-    });
-    this.requests[0].respond(400, { 'Content-Type': 'application/json' }, JSON.stringify(error));
-  });
-  it('Testiranje posaljiPodatke 4', function (done) {
-    let data = {
-      "brojVjezbi": 0,
-      "brojZadataka": []
-    }
-    let error = {
-      "status": "error",
-      "data": "Pogrešan parametar brojVjezbi"
-  }
-    VjezbeAjax.posaljiPodatke(data, function (err, result) {
-      chai.assert(result === null);
-      done();
-    });
-    this.requests[0].respond(400, { 'Content-Type': 'application/json' }, JSON.stringify(error));
-  });
-  it('Testiranje posaljiPodatke 5', function (done) {
-    let data = {
-      "brojVjezbi": -6,
-      "brojZadataka": []
-    }
-    let error = {
-      "status": "error",
-      "data": "Pogrešan parametar brojVjezbi,brojZadataka"
-  }
-    VjezbeAjax.posaljiPodatke(data, function (err, result) {
-      chai.assert(result === null);
-      done();
-    });
-    this.requests[0].respond(400, { 'Content-Type': 'application/json' }, JSON.stringify(error));
-  });
-  it('Testiranje posaljiPodatke 6', function (done) {
-    let data = {
-      "brojVjezbi": 0,
-      "brojZadataka": [1,2,-2,-3,0,10,90,1]
-    }
-    let error = {
-      "status": "error",
-      "data": "Pogrešan parametar brojVjezbi,z2,z3,z4,z6,brojZadataka"
-  }
-    VjezbeAjax.posaljiPodatke(data, function (err, result) {
-      chai.assert(result === null);
-      done();
-    });
-    this.requests[0].respond(400, { 'Content-Type': 'application/json' }, JSON.stringify(error));
-  });
+      let dataJson = JSON.stringify(data)
+      VjezbeAjax.posaljiPodatke(null, function (err, result) {
+        result.should.deep.equal(dataJson);
+        done();
+      });
 
-  it('Testiranje dodajInputPolja 1', function (done) {
-    
-   
-  });
+      this.requests[0].respond(200, { 'Content-Type': 'text/json' }, dataJson);
+    });
+    it('Veca lista od broja vjezbi', function (done) {
+      let data = {
+        "brojVjezbi": 5,
+        "brojZadataka": [1, 2, 9, 6, 4, -1, 0]
+      }
+      let error = {
+        "status": "error",
+        "data": "Pogrešan parametar brojZadataka"
+      }
+      VjezbeAjax.posaljiPodatke(data, function (err, result) {
+        chai.assert(result === null);
+        done();
+      });
+      this.requests[0].respond(400, { 'Content-Type': 'application/json' }, JSON.stringify(error));
+    });
+    it('Broj vjezbi 0 sa nepraznom listom', function (done) {
+      let data = {
+        "brojVjezbi": 0,
+        "brojZadataka": [1, 2, 9, 6, 4, -1, 0]
+      }
+      let error = {
+        "status": "error",
+        "data": "Pogrešan parametar brojVjezbi,brojZadataka"
+      }
+      VjezbeAjax.posaljiPodatke(data, function (err, result) {
+        chai.assert(result === null);
+        done();
+      });
+      this.requests[0].respond(400, { 'Content-Type': 'application/json' }, JSON.stringify(error));
+    });
+    it('Broj vjezbi 0 sa praznom listom', function (done) {
+      let data = {
+        "brojVjezbi": 0,
+        "brojZadataka": []
+      }
+      let error = {
+        "status": "error",
+        "data": "Pogrešan parametar brojVjezbi"
+      }
+      VjezbeAjax.posaljiPodatke(data, function (err, result) {
+        chai.assert(result === null);
+        done();
+      });
+      this.requests[0].respond(400, { 'Content-Type': 'application/json' }, JSON.stringify(error));
+    });
+    it('Negativan broj vjezbi', function (done) {
+      let data = {
+        "brojVjezbi": -6,
+        "brojZadataka": []
+      }
+      let error = {
+        "status": "error",
+        "data": "Pogrešan parametar brojVjezbi,brojZadataka"
+      }
+      VjezbeAjax.posaljiPodatke(data, function (err, result) {
+        chai.assert(result === null);
+        done();
+      });
+      this.requests[0].respond(400, { 'Content-Type': 'application/json' }, JSON.stringify(error));
+    });
+    it('Negativni i prekoračeni brojevi zadataka', function (done) {
+      let data = {
+        "brojVjezbi": 8,
+        "brojZadataka": [1, 2, -2, -3, 0, 10, 90, 1]
+      }
+      let error = {
+        "status": "error",
+        "data": "Pogrešan parametar z2,z3,z4,z6"
+      }
+      VjezbeAjax.posaljiPodatke(data, function (err, result) {
+        chai.assert(result === null);
+        done();
+      });
+      this.requests[0].respond(400, { 'Content-Type': 'application/json' }, JSON.stringify(error));
+    });
+  })
+
+  describe('Testiranje dodajInputPolja', function () {
+
+    it('Negativan broj polja', function (done) {
+      let div = document.createElement('div')
+      VjezbeAjax.dodajInputPolja(div, -10);
+      div.innerHTML.should.deep.eq('')
+      done();
+    });
+    it('Broj polja > 15', function (done) {
+      let div = document.createElement('div')
+      VjezbeAjax.dodajInputPolja(div, 910);
+      div.innerHTML.should.deep.eq('')
+      done();
+
+    });
+    it('Broj polja == 0', function (done) {
+      let div = document.createElement('div')
+      VjezbeAjax.dodajInputPolja(div, 0);
+      div.innerHTML.should.deep.eq('')
+      done();
+    });
+    it('Ispravan broj polja', function (done) {
+      let div = document.createElement('div')
+      VjezbeAjax.dodajInputPolja(div, 2);
+      div.innerHTML.should.deep.eq('<div><label for="z0">Unesite broj zadataka za vjezbu 1:</label><input type="number" name="z0" id="z0" value="4"></div><div><label for="z1">Unesite broj zadataka za vjezbu 2:</label><input type="number" name="z1" id="z1" value="4"></div>')
+      done();
+    });
+  })
+
+  describe('Testiranje dohvatiPodatke', function(){
+    it('Dohvatanje podataka', function (done) {
+      const data = {
+        brojVjezbi: 5,
+        brojZadataka: [1, 2, 3, 4, 5],
+      }
+      let dataJson = JSON.stringify(data)
+      VjezbeAjax.dohvatiPodatke(function (err, result) {
+        result.should.deep.equal(dataJson);
+        done();
+      });
+
+      this.requests[0].respond(200, { 'Content-Type': 'text/json' }, dataJson);
+    })
+  })
+  describe('Testiranje iscrtajVjezbe', function () {
+    it('Isparavan format', function (done) {
+      let div = document.createElement('div')
+      const data = {
+        brojVjezbi: 5,
+        brojZadataka: [1, 2, 3, 4, 5],
+      }
+
+      let dataJson = JSON.stringify(data)
+      VjezbeAjax.iscrtajVjezbe(div, data)
+      div.innerHTML.should.deep.eq('<div class="vjezba" id="v0">Vjezba 1</div><div class="zadaci" style="display: none;"><div class="zadatak" id="z0">Zadatak 1</div></div><div class="vjezba" id="v1">Vjezba 2</div><div class="zadaci" style="display: none;"><div class="zadatak" id="z0">Zadatak 1</div><div class="zadatak" id="z1">Zadatak 2</div></div><div class="vjezba" id="v2">Vjezba 3</div><div class="zadaci" style="display: none;"><div class="zadatak" id="z0">Zadatak 1</div><div class="zadatak" id="z1">Zadatak 2</div><div class="zadatak" id="z2">Zadatak 3</div></div><div class="vjezba" id="v3">Vjezba 4</div><div class="zadaci" style="display: none;"><div class="zadatak" id="z0">Zadatak 1</div><div class="zadatak" id="z1">Zadatak 2</div><div class="zadatak" id="z2">Zadatak 3</div><div class="zadatak" id="z3">Zadatak 4</div></div><div class="vjezba" id="v4">Vjezba 5</div><div class="zadaci" style="display: none;"><div class="zadatak" id="z0">Zadatak 1</div><div class="zadatak" id="z1">Zadatak 2</div><div class="zadatak" id="z2">Zadatak 3</div><div class="zadatak" id="z3">Zadatak 4</div><div class="zadatak" id="z4">Zadatak 5</div></div>')
+      done();
+    });
+    it('Negativan broj vjezbi', function (done) {
+      let div = document.createElement('div')
+      const data = {
+        brojVjezbi: -5,
+        brojZadataka: [1, 2, 3, 4, 5],
+      }
+      VjezbeAjax.iscrtajVjezbe(div, data)
+      div.innerHTML.should.deep.eq('')
+      done();
+    });
+    it('Broj vjezbi > 15', function (done) {
+      let div = document.createElement('div')
+      const data = {
+        brojVjezbi: 25,
+        brojZadataka: [1, 2, 3, 4, 5],
+      }
+      VjezbeAjax.iscrtajVjezbe(div, data)
+      div.innerHTML.should.deep.eq('')
+      done();
+    });
+    it('Broj vjezbi == 0', function (done) {
+      let div = document.createElement('div')
+      const data = {
+        brojVjezbi: 0,
+        brojZadataka: [1, 2, 3, 4, 5],
+      }
+      VjezbeAjax.iscrtajVjezbe(div, data)
+      div.innerHTML.should.deep.eq('')
+      done();
+    });
+    it('Negativni brojevi zadataka', function (done) {
+      let div = document.createElement('div')
+      const data = {
+        brojVjezbi: 5,
+        brojZadataka: [1, -2, -3, -4, 5],
+      }
+      VjezbeAjax.iscrtajVjezbe(div, data)
+      div.innerHTML.should.deep.eq('')
+      done();;
+    });
+    it('Prekoračeni brojevi vjezbi', function (done) {
+      let div = document.createElement('div')
+      const data = {
+        brojVjezbi: 35,
+        brojZadataka: [1, -2, -3, -4, 5],
+      }
+      VjezbeAjax.iscrtajVjezbe(div, data)
+      div.innerHTML.should.deep.eq('')
+      done();;
+    });
+    it('Prekoračeni brojevi zadataka', function (done) {
+      let div = document.createElement('div')
+      const data = {
+        brojVjezbi: 15,
+        brojZadataka: [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+      }
+      VjezbeAjax.iscrtajVjezbe(div, data)
+      div.innerHTML.should.deep.eq('')
+      done();;
+    });
+  })
 
 });
--- public/js/vjezbeAjax.js
+++ public/js/vjezbeAjax.js
@@ -9,7 +9,7 @@ let VjezbeAjax = (function(){
                 let div = document.createElement('div');
                 div.innerHTML = `<label for="z${i}">Unesite broj zadataka za vjezbu ${i+1}:</label>` + `<input type="number" name="z${i}" id="z${i}" value="4">`;
                 div.getElementsByTagName('input').value = 4;
-                $('#polja').append(div);
+                DOMelementDIVauFormi.append(div);
             }
         }
     }
@@ -23,8 +23,8 @@ let VjezbeAjax = (function(){
             console.log(this);
             if (this.readyState == 4 && this.status == 200) {
                 console.log('oke')
-                callbackFunkcija(null, this.responseText)               // ! todo  provjeriti za parametre funckije
-            }else{
+                callbackFunkcija(null, this.responseText)           
+            }else if(this.readyState == 4){
                 console.log('nije oke')
                 callbackFunkcija(this.responseText , null) 
             }
@@ -41,34 +41,35 @@ let VjezbeAjax = (function(){
         xhr.onreadystatechange = function () {
             console.log(this.responseText);
             if (this.readyState == 4 && this.status == 200) {
-                callbackFunkcija(null, this.responseText)               // ! todo  provjeriti za parametre funckije
-            }else{
+                callbackFunkcija(null, this.responseText)               
+            }else if(this.readyState == 4){
                 callbackFunkcija(this.responseText , null) 
             }
         };
         xhr.onload = function() {
             console.log(this.responseText);
-            
         }
         xhr.send();
     }
     
     const iscrtajVjezbe = function(divDOMelement, obj){
-        for(let i = 0; i < obj.brojVjezbi; i++){
-            let div = document.createElement('div')
-            div.classList.add('vjezba')
-            div.id =`v${i}`
-            div.innerText = `Vjezba ${i+1}`
-            let innerDiv = document.createElement('div');
-            innerDiv.classList.add('zadaci')
-            //div.appendChild(innerDiv)
-            iscrtajZadatke(innerDiv,obj.brojZadataka[i])
-            div.addEventListener('click', function(){
-                iscrtajZadatke(innerDiv, -1)                    // ! HARDKODIRANO
-            })
-            listaVjezbi.push(innerDiv)
-            divDOMelement.append(div)
-            divDOMelement.append(innerDiv)
+        if(obj.brojVjezbi == obj.brojZadataka.length && obj.brojZadataka.every(it=> it > 0) && obj.brojVjezbi <=15 && obj.brojZadataka.length <=10){
+            for(let i = 0; i < obj.brojVjezbi; i++){
+                let div = document.createElement('div')
+                div.classList.add('vjezba')
+                div.id =`v${i}`
+                div.innerText = `Vjezba ${i+1}`
+                let innerDiv = document.createElement('div');
+                innerDiv.classList.add('zadaci')
+                //div.appendChild(innerDiv)
+                iscrtajZadatke(innerDiv,obj.brojZadataka[i])
+                div.addEventListener('click', function(){
+                    iscrtajZadatke(innerDiv, -1)
+                })
+                listaVjezbi.push(innerDiv)
+                divDOMelement.append(div)
+                divDOMelement.append(innerDiv)
+            }
         }
     }
     const iscrtajZadatke = function(divDOMelement, brojVjezbi){
@@ -76,16 +77,17 @@ let VjezbeAjax = (function(){
             for(let i = 0; i < brojVjezbi; i++){
                 let div = document.createElement('div')
                 div.classList.add('zadatak')
-                div.id =`z${i}`
+                 div.id =`z${i}`
                 div.innerText = `Zadatak ${i+1}`
                 divDOMelement.style.display = 'none'
                 divDOMelement.append(div)
-               
             }    
         }else{
             for(let ii in listaVjezbi){
-                if(listaVjezbi[ii].style.display === 'flex')
+                if(listaVjezbi[ii].style.display === 'flex'){
                     listaVjezbi[ii].style.display = 'none'
+                    console.log('kliknuto' + ii)
+                }
             }
             if(divDOMelement.style.display === 'flex')
                 divDOMelement.style.display = 'none';
